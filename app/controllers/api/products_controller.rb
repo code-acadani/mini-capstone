85cd73a9a2fdbf97438a0e4a91ee5c2b74ac18cb
dani <dani.zaghian@gmail.com>
2018-09-04 11:21:52 -0700

    adds jbuilder partial

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,11 +1,40 @@
 class Api::ProductsController < ApplicationController
-	def all_products_method
-		@products = Product.all
-		render 'all_products.json.jbuilder'
+	def index
+	  @products = Product.all
+	  render 'index.json.jbuilder'
 	end
 
-	def one_product
-		@product = Product.find_by(id: params["id"])
-		render 'one_product.json.jbuilder'
+	def create
+	  @product = Product.new(
+	   name: params[:name],
+	   price: params[:price],
+	   description: params[:description],
+	   image_url: params[:image_url]
+	  )
+	  @product.save
+	  render 'show.json.jbuilder'
+	end
+
+	def show
+	  @product = Product.find(params[:id])
+	  render 'show.json.jbuilder'
+	end
+
+	def update
+	  @product = Product.find(params[:id])
+	  
+	  @product.name = params[:name] || @product.name
+	  @product.price = params[:price] || @product.price
+	  @product.description = params[:description] || @product.description
+	  @product.image_url = params[:image_url] || @product.image_url
+
+	  @product.save
+	  render 'show.json.jbuilder'
+	end
+
+	def destroy
+	  @product = Product.find(params[:id])
+	  @product.destroy
+	  render json: {message: "Product successfully destroyed"}
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
   namespace :api do
-  	get "/all_products_url" => "products#all_products_method"
-
-  	get "/one_product" => "products#one_product"
-
-  	get "/one_product/:id" => "products#one_product"
+    get "/products" => 'products#index'
+    post "/products" => 'products#create'
+    get "/products/:id" => 'products#show'
+    patch "/products/:id" => 'products#update'
+    delete "/products/:id" => 'products#destroy'
   end
 end
